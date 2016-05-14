cb49b14fa30efb9d6a060687892c8cd48a81849a
Bhupinder Jeet Singh <bhupinder@blitsgoa.com>
2016-05-14 11:26:25 +0530

    modified and fixed footer issue

--- index.php
+++ index.php
@@ -3,4 +3,5 @@ echo "This is a tutorial for github";
 echo "Updating this file";
 echo "Modified index.php file";
 echo "Modified index.php file. added new line of code";
+echo "finished new footer issue[issue 53]";
 ?>
